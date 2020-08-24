d87d601a5a8e9324381cd5b45c37bdefc5519d68
Tuan Pham <tuan.pham@agiletech.vn>
2020-08-24 21:48:49 +0700

    initialization commit

---
+++ src/layouts/AppLayout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const AppLayout = ({ children }) => (
+  <div>
+    {children}
+  </div>
+);
+
+export default AppLayout;
+
---
+++ src/pages/map/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppLayout from '@/layouts/AppLayout';
+
+const Map = () => (
+  <AppLayout>
+    <div />
+  </AppLayout>
+);
+
+export default Map;
